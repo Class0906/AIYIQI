0e8be8034f38818115ed81bf93fe8361f1417d16
CodingMing4XQ <125975490@qq.com>
2016-11-25 01:21:49 +0800

    just for commit

--- app/src/main/java/com/bwf/aiyiqi/gui/adapter/MainRecyclerViewAdapter.java
+++ app/src/main/java/com/bwf/aiyiqi/gui/adapter/MainRecyclerViewAdapter.java
@@ -5,11 +5,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.bwf.aiyiqi.R;
 import com.bwf.aiyiqi.entity.ResponseHomeBBS;
+import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by lmw on 2016/11/24.
@@ -28,26 +35,37 @@ public class MainRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.V
         inflater = LayoutInflater.from(context);
     }
 
-    public void addData(){
+    public void addData() {
         data.addAll(data);
         notifyDataSetChanged();
     }
 
     @Override
     public int getItemViewType(int position) {
-        if(position == 0)
+        if (position == 0)
             return TYPE_HEADER;
-        if (position == getItemCount()-1)
+        if (position == getItemCount() - 1)
             return TYPE_FOOTER;
-        if (data.get(position).getType()==1){
+        if (data.get(position).getType() == 1) {
             return TYPE_ARTICLE;
-        }else
+        } else
             return TYPE_BBS;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
+        if (viewType == TYPE_HEADER) {
+            View headerView = inflater.inflate(R.layout.item_header_main_recycler, parent, false);
+            return new HeaderViewHolder(headerView);
+        }
+        if (viewType == TYPE_FOOTER) {
+            View footerView = inflater.inflate(R.layout.item_footer_main_recycler, parent, false);
+            return new HeaderViewHolder(footerView);
+        }
+        if (viewType == TYPE_ARTICLE) {
+            View articleView = inflater.inflate(R.layout.item_article_main_recycler, parent, false);
+            return new HeaderViewHolder(articleView);
+        }
         return null;
     }
 
@@ -58,7 +76,7 @@ public class MainRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.V
 
     @Override
     public int getItemCount() {
-        return data.size()+getHeaderCount()+getFooterCount();
+        return data.size() + getHeaderCount() + getFooterCount();
     }
 
     private int getHeaderCount() {
@@ -69,10 +87,59 @@ public class MainRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.V
         return 1;
     }
 
-    private class HeaderViewHolder extends RecyclerView.ViewHolder{
+    public class HeaderViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.layout_main_header_company)
+        LinearLayout layoutMainHeaderCompany;
+        @BindView(R.id.layout_main_header_activity)
+        LinearLayout layoutMainHeaderActivity;
+        @BindView(R.id.layout_main_header_school)
+        LinearLayout layoutMainHeaderSchool;
+        @BindView(R.id.layout_main_header_money)
+        LinearLayout layoutMainHeaderMoney;
+        @BindView(R.id.layout_main_header_category)
+        LinearLayout layoutMainHeaderCategory;
+        @BindView(R.id.layout_main_header_picture)
+        LinearLayout layoutMainHeaderPicture;
+        @BindView(R.id.layout_main_header_order)
+        LinearLayout layoutMainHeaderOrder;
+        @BindView(R.id.layout_main_header_design)
+        LinearLayout layoutMainHeaderDesign;
 
         public HeaderViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public class FooterViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.text_loading)
+        TextView textLoading;
+        @BindView(R.id.textview_nomore_data)
+        TextView textviewNomoreData;
+        @BindView(R.id.text_reload)
+        TextView textReload;
+
+        public FooterViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public class ArticleViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.text_main_recycler_item_title)
+        TextView textMainRecyclerItemTitle;
+        @BindView(R.id.image_main_recycler_item)
+        SimpleDraweeView imageMainRecyclerItem;
+        @BindView(R.id.text_publish_time)
+        TextView textPublishTime;
+        @BindView(R.id.text_comment_count)
+        TextView textCommentCount;
+        @BindView(R.id.text_looked_count)
+        TextView textLookedCount;
+
+        ArticleViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
