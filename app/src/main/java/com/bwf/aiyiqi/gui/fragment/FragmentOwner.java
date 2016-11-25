f59df403068bf427c7010942266d4cbc4ffb3c6b
pengyanliGit <1285604993@qq.com>
2016-11-25 11:00:05 +0800

    pengyanli commit for owner please ignore

--- app/src/main/java/com/bwf/aiyiqi/gui/fragment/FragmentOwner.java
+++ app/src/main/java/com/bwf/aiyiqi/gui/fragment/FragmentOwner.java
@@ -6,17 +6,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.bwf.aiyiqi.R;
+
+import java.util.List;
 
 /**
  * Created by Zoulin on 2016/11/23.
  */
 
 public class FragmentOwner extends Fragment {
+
+    private static final String ARG_Page = "ARG_Page";
+    private List<String> owner_title;
+    private List<Fragment> owner_list_fragment;
+    private int pageNum;
+
+    public static FragmentOwner newInstance(int pageNum){
+        Bundle bundle = new Bundle(  );
+        bundle.putInt( ARG_Page,pageNum );
+        FragmentOwner fragmentOwner = new FragmentOwner();
+        fragmentOwner.setArguments( bundle );
+        return fragmentOwner;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        pageNum = getArguments().getInt( ARG_Page );
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_owner,null);
+       View view = inflater.inflate( R.layout.fragment_owner,container,false );
+        TextView textView = (TextView) view;
+        textView.setText( "Fragment  #" + pageNum+"  #" );
+        return view;
     }
+
+
+
 }
